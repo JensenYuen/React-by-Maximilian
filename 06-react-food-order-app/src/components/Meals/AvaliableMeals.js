48abe5814c86139ed220ab1c4d47be267826a390
jensen yuen <jensenyuen@outlook.com>
2022-02-28 18:15:21 +0800

    imported meals compoment

--- 06-react-food-order-app/src/components/Meals/AvaliableMeals.js
+++ 06-react-food-order-app/src/components/Meals/AvaliableMeals.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
--- 06-react-food-order-app/src/components/Meals/Meals.js
+++ 06-react-food-order-app/src/components/Meals/Meals.js
@@ -1,7 +1,15 @@
-import React from "react";
+import { Fragment} from "react";
+
+import MealsSummary from "./MealsSummary";
+import AvaliableMeals from "./AvaliableMeals";
 
 const Meals = () => {
-
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvaliableMeals />
+    </Fragment>
+  );
 };
 
 export default Meals;
