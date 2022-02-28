748a04f8eb3fc8c9a0ea79ba578ec8db78f8b777
jensen yuen <jensenyuen@outlook.com>
2022-03-01 04:11:22 +0800

    outputting cart items

--- 06-react-food-order-app/src/store/CartProvider.js
+++ 06-react-food-order-app/src/store/CartProvider.js
@@ -9,8 +9,25 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.items);
     const updatedTotalAmount = state.totalAmount + action.items.price * action.items.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.items.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.items.amount
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.items);
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
