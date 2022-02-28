1f28e36917509ea6fe22ee2095375c10d8942637
jensen yuen <jensenyuen@outlook.com>
2022-02-28 18:31:56 +0800

    import MealItem component

--- 06-react-food-order-app/src/components/Meals/AvaliableMeals.js
+++ 06-react-food-order-app/src/components/Meals/AvaliableMeals.js
@@ -1,5 +1,8 @@
 import classes from "./AvailableMeals.module.css";
 
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -30,18 +33,21 @@ const DUMMY_MEALS = [
 const AvaliableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => {
     return (
-      <li key={meal.key}>
-        {meal.name}
-        {meal.price}
-        <span>{meal.description}</span>
-      </li>
+      <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
     );
   });
 
   return (
     <section className={classes.meals}>
       <ul>
-        {mealsList}
+        <Card>
+          {mealsList}
+        </Card>
       </ul>
     </section>
   );
--- 06-react-food-order-app/src/components/Meals/MealsItem.js
+++
@@ -1,9 +0,0 @@
-import classes from "./MealsItem.module.css"
-
-const MealsItem = (props) => {
-  return (
-    {props.name}
-  );
-};
-
-export default MealsItem;
