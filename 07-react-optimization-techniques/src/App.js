e8a8bee3efda6538e0e72c1ce3b628e4c3605946
jensen yuen <jensenyuen@outlook.com>
2022-03-04 13:48:00 +0800

    component updates

--- 07-react-optimization-techniques/src/App.js
+++ 07-react-optimization-techniques/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Button from "./components/UI/Button/Button"
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const ToggleShowParagraph = () => {
+    setShowParagraph((PrevShowParagraph) => !PrevShowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={ToggleShowParagraph}>Show paragraph</Button>
     </div>
   );
 }
